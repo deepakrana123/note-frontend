4f7b55c74eea6d25de878d88099de45ee36c10fd
deepakrana123 <rdev69146@gmail.com>
2023-01-06 16:52:53 +0530

    k

--- src/components/MyNotes/MyNotes.js
+++ src/components/MyNotes/MyNotes.js
@@ -1,4 +1,4 @@
-import React,{useEffect ,useState} from 'react'
+import React,{useState} from 'react'
 import NoteCard from '../NoteCard/NoteCard';
 import axios from "axios";
 import "./MyNotes.css"
